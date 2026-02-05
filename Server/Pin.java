cf0f100d8416255363b77964d8515457ee749cda
Roryewing <roryaewing12@gmail.com>
2026-02-05 18:06:55 -0500

    Add getters and toString method to Pin class

--- Server/Pin.java
+++ Server/Pin.java
@@ -1,4 +1,7 @@
 public class Pin {
     public int x, y;
     public Pin(int x, int y) { this.x = x; this.y = y; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    @Override public String toString() { return "PIN " + x + " " + y; }
 }
